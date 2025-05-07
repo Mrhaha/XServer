f132dc7751dc2acd98a4f3e3e488c51beb59b329
cole <1091953018@qq.com>
2025-05-07 17:42:38 +0800

    commit echo server by asio

---
+++ cmake-build-debug/CMakeFiles/echo_client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/echo_client.dir/src/echoClient/main.cpp.o"
+  "CMakeFiles/echo_client.dir/src/echoClient/main.cpp.o.d"
+  "echo_client"
+  "echo_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/echo_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/echo_server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/echo_server.dir/src/echoServer/echo_server.cpp.o"
+  "CMakeFiles/echo_server.dir/src/echoServer/echo_server.cpp.o.d"
+  "CMakeFiles/echo_server.dir/src/echoServer/main.cpp.o"
+  "CMakeFiles/echo_server.dir/src/echoServer/main.cpp.o.d"
+  "echo_server"
+  "echo_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/echo_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
